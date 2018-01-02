5cea4119b1a2d0c84a2168b23edd12c904776916
chenzihao <523108257@qq.com>
2018-01-02 15:15:00 +0800

    dagger2demo

--- app/src/main/java/com/demo/czh/czhdagger2demo/example/di/UserFramentComponent.kt
+++ app/src/main/java/com/demo/czh/czhdagger2demo/example/di/UserFramentComponent.kt
@@ -1,14 +1,11 @@
 package com.demo.czh.czhdagger2demo.example.di
 
-import com.demo.czh.czhdagger2demo.example.di.scope.ActivityScope
-import com.demo.czh.czhdagger2demo.example.di.scope.FragmentScope
 import com.demo.czh.czhdagger2demo.example.view.UserFragment
 import dagger.Subcomponent
 
 /**
  * Created by hasee on 2017/12/19.
  */
-@FragmentScope
 @Subcomponent
 interface UserFramentComponent {
     fun inject(userFragment: UserFragment)
